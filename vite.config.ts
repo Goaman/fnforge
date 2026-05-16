791dd94322f6ee2e1683b86d75a541b1089240a1
Goaman <Goaman@users.noreply.github.com>
2026-05-16 10:37:52 +0000

    deploy: 851391fe69002f514c178b5ce320eff1bbf2d0e7

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 
 export default defineConfig({
+  base: '/fnforge/',
   build: {
     emptyOutDir: true,
     outDir: '../dist',
